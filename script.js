cb9252ad16eb9a25ba0c9a8f7273ecaf841f7a65
Fish-A-code <kentdaniel777@yahoo.com>
2021-05-05 17:53:11 +0700

    added infinite scroll functionality + error card

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const imageContainer = document.getElementById('image-container');
 const loader= document.getElementById('loader')
+const error= document.getElementById('error');
 
 let photosArray=[]
+let ready=false;
+let imageLoaded=0;
+let totalImage=0;
+
 // Unsplash API
-const count=10
+const count=30;
 const apiKey="UbXc2ciaLfuxlnPAazlz-tuGvJvASVi40LJp0BbvQHM";
 const unsplashAPI=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -15,16 +20,44 @@ function setAttributes(element,attr){
     }
 }
 
+// check if all the image is loaded before calling another getPhotos()
+function imageLoad(){
+    imageLoaded++;
+    // console.log(imageLoaded,totalImage,ready)
+    if(imageLoaded===totalImage){
+        ready=true;
+        removeLoader();
+    }
+
+}
+
+// show loading animation
+function showLoader(){
+    console.log("loading")
+    loader.hidden=false;
+}
+
+function removeLoader(){
+    console.log("finished loading");
+    loader.hidden=true;
+}
+
+
 // create elements for the DOM
-function displayPhotos(photosArray){
-    photosArray.forEach((photo)=>{
+ function displayPhotos(photosArray){
+
+    imageLoaded=0;
+    totalImage=photosArray.length;
+
+    showLoader();
+    photosArray.forEach(async (photo)=>{
         // add the <a> tag
         const item = document.createElement("a");
         setAttributes(item,{
             "href":photo.links.html,
             "target":"_blank"
         });
-        
+
         // add the img
         const img = document.createElement('img');
         setAttributes(img,{
@@ -32,7 +65,9 @@ function displayPhotos(photosArray){
             "title":photo.alt_description,
             "alt":photo.alt_description,
         });
-
+        
+        img.addEventListener("load",imageLoad);
+        
         // add the img in the <a> and both to the image container
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -42,13 +77,26 @@ function displayPhotos(photosArray){
 // get photos from API
 async function getPhotos(){
     try{
+        error.hidden=true;
+        photosArray=[];
         const response = await fetch(unsplashAPI);
         photosArray=await response.json();
         displayPhotos(photosArray);
     }catch(err){
-        console.log(err)
+        console.log(err.name)
+        error.hidden=false;
     }
     
    
 }
 
+getPhotos();
+window.addEventListener("scroll",()=>{
+    if (window.scrollY+window.innerHeight>= document.body.offsetHeight -1000 && ready){
+        console.log("get photos")
+        getPhotos();
+        ready=false;
+        showLoader();
+    }
+  
+})
